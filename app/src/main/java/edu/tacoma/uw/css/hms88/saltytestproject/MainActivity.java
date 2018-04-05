879958626ab69f6e237d4914d7cc054eb60b8061
saltybard <cbr.work@hotmail.com>
2018-04-05 14:10:47 -0700

    Doing some trial stuff

--- app/src/main/java/edu/tacoma/uw/css/hms88/saltytestproject/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/hms88/saltytestproject/MainActivity.java
@@ -2,12 +2,21 @@ package edu.tacoma.uw.css.hms88.saltytestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: onStart");
     }
 }
