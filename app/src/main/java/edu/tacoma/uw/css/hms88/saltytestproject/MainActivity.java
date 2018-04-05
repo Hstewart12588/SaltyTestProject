4fc5760afa15e1e88bc35d05c4a4294e88ef3edb
Exeter <stephanie_Day8@msn.com>
2018-04-05 14:15:51 -0700

    Beep Boop I added a Log.

--- app/src/main/java/edu/tacoma/uw/css/hms88/saltytestproject/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/hms88/saltytestproject/MainActivity.java
@@ -1,13 +1,24 @@
 package edu.tacoma.uw.css.hms88.saltytestproject;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String STEPH_TAG = "STEPHANIE: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        Log.d(STEPH_TAG, "Beep Boop I changed the code.");
+    }
+
+
 }
